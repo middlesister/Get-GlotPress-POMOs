cf902f61977fa95853343a43ea1638cbfa4b4bb2
emhr <gene@emhr.info>
2013-03-05 13:18:57 -0500

    Initial conversion to pull mos and pos from translate.thematictheme.com

--- get-translate-pomos.rb
+++ get-translate-pomos.rb
@@ -5,7 +5,7 @@ require 'open-uri'
 require 'net/http'
 
 =begin
-Downloads .mo and .pot files from completed translations of a specific project on translate.wordpress.org.
+Downloads .mo and .po files from the translations of a specific project on translate.thematictheme.com.
 
 
 Copyright (c) 2012 Paul Gibbs
@@ -18,7 +18,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 # If required parameters are missing, bail out
 if ARGV[0].nil? or ARGV[1].nil?
 	puts 'Usage: get-translate-pomos.rb PROJECT VERSION'
-	puts "\tPROJECT: URL slug for a project listed at http://translate.wordpress.org/projects/. e.g. buddypress"
+	puts "\tPROJECT: URL slug for a project listed at http://translate.thematictheme.com/projects/. "
 	puts "\tVERSION: A sub-project, or version, of the PROJECT. e.g. 1.6.x"
 	puts ''
 	abort
@@ -29,9 +29,9 @@ translations = []
 
 # Load the appropriate project page
 begin
-	doc = Hpricot( open( "https://translate.wordpress.org/projects/#{ARGV[0]}/#{ARGV[1]}" ).read )
+	doc = Hpricot( open( "http://translate.thematictheme.com/projects/#{ARGV[0]}/#{ARGV[1]}" ).read )
 rescue => error
-	abort( "An error occured when trying to load https://translate.wordpress.org/projects/#{ARGV[0]}/#{ARGV[1]}: #{error}" )
+    abort( "An error occured when trying to load http://translate.thematictheme.com/projects/#{ARGV[0]}/#{ARGV[1]}: #{error}" )
 end
 
 # Iterate through each table row
@@ -41,8 +41,8 @@ doc.search( '.translation-sets tbody tr' ).each do |row|
 	lang_code  = base_url.split( '/' ).fetch( -2 ).gsub( /[^aA0-zZ9.\-]/, '_' );
 	language   = row.search( 'td strong a' ).inner_html.gsub( /\n/, '' )
 
-	# Let's only work with translations that are at least 90% complete
-	if completion.to_i < 90
+	# Let's only work with translations that are at least 10% complete
+	if completion.to_i < 10
 		next
 	end
 
@@ -55,12 +55,12 @@ translation_maps = [
 ];
 
 # Open a HTTP connection
-Net::HTTP.start( 'translate.wordpress.org' ) do |http|
+Net::HTTP.start( 'translate.thematictheme.com' ) do |http|
 	begin
 
 		# Create output directory exists
 		begin
-			Dir.mkdir( File.join( Dir.pwd, 'pomo' ) )
+			Dir.mkdir( File.join( Dir.pwd, 'languages' ) )
 		rescue
 		end
 
@@ -71,40 +71,64 @@ Net::HTTP.start( 'translate.wordpress.org' ) do |http|
 			# See if there's a way to automagically figure this out from GlotPress -- or build a big hashmap
 			file_name = translation[:lang_code];
 			case file_name
-			when 'es'
-				file_name = 'es_ES'
-			when 'nl'
-				file_name = 'nl_NL'
-			when 'ko'
-				file_name = 'ko_KR'
-			when 'ru'
-				file_name = 'ru_RU'
-			when 'pt-br'
-				file_name = 'pt_BR'
-			when 'pt'
-				file_name = 'pt_PT'
-			when 'nb'
-				file_name = 'nb_NO'
-			when 'it'
-				file_name = 'it_IT'
-			when 'th'
-				file_name = 'th'
-			when 'sk'
-				file_name = 'sk_SK'
-			when 'fr'
-				file_name = 'fr_FR'
-			when 'hu'
-				file_name = 'hu_HU'
-			when 'da'
+			when 'cs'
+				file_name = 'cs_CZ'
+            when 'da'
 				file_name = 'da_DK'
-			when 'zh-tw'
-				file_name = 'zh_TW'
+            when 'de'
+				file_name = 'de_DE'
+            when 'es'
+				file_name = 'es_ES'
+            when 'fa'
+				file_name = 'fa_IR'
+            when 'fr'
+				file_name = 'fr_FR'
+            when 'he'
+				file_name = 'he_IL'
+            when 'hu'
+				file_name = 'hu_HU'
+            when 'id'
+				file_name = 'id_ID'
+            when 'is'
+				file_name = 'is_IS'
+            when 'it'
+				file_name = 'it_IT'
+            when 'ko'
+				file_name = 'ko_KR'
+            when 'nb'
+				file_name = 'nb_NO'
+            when 'nl'
+				file_name = 'nl_NL'
+            when 'pl'
+				file_name = 'pl_PL'
+            when 'pt-br'
+				file_name = 'pt_BR'
+            when 'pt'
+				file_name = 'pt_PT'
+            when 'ro'
+				file_name = 'ro_RO'
+            when 'ru'
+				file_name = 'ru_RU'
+            when 'sk'
+				file_name = 'sk_SK'
+            when 'sr'
+				file_name = 'sr_RS'
+            when 'sv'
+				file_name = 'sv_SE'
+            when 'tr'
+				file_name = 'tr_TR'
+			when 'zh-cn'
+				file_name = 'zh_CN'
+			when 'zh-hk'
+				file_name = 'zh_HK'
+            when 'zh-tw'
+				file_name = 'zh-TW'
 			else
 				file_name = file_name
 			end
 
-			file_mo   = open( File.join( Dir.pwd, 'pomo', "#{file_name}.mo" ), 'wb' )
-			file_pot  = open( File.join( Dir.pwd, 'pomo', "#{file_name}.pot" ), 'wb' )
+			file_mo   = open( File.join( Dir.pwd, 'languages', "#{file_name}.mo" ), 'wb' )
+			file_po  = open( File.join( Dir.pwd, 'languages', "#{file_name}.po" ), 'wb' )
 
 			# Download the .mo. We're writing straight to the open file rather than buffering contents in memory
 			http.request_get( "#{translation[:base_url]}/export-translations?format=mo" ) do |response|
@@ -113,15 +137,15 @@ Net::HTTP.start( 'translate.wordpress.org' ) do |http|
 				end
 			end
 
-			# Download the .pot. We're writing straight to the open file rather than buffering contents in memory
+			# Download the .po. We're writing straight to the open file rather than buffering contents in memory
 			http.request_get( "#{translation[:base_url]}/export-translations?format=po" ) do |response|
 				response.read_body do |segment|
-					file_pot.write( segment )
+					file_po.write( segment )
 				end
 			end
 
 			file_mo.close
-			file_pot.close
+			file_po.close
 		end
 
 	end
